4a0fee4b583918b859de15a89317e1628101678f
gothlul <lrasoppi11@gmail.com>
2024-01-22 22:51:31 -0300

    22/01/2024

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $senha = 'usbw';
+    $banco = 'catalogo';
+
+    $connect = mysqli_connect($host, $user, $senha, $banco);
+?>
--- pages/index.php
+++ pages/index.php
@@ -3,15 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stream+</title>
+    <title>Amethyst | Sua plataforma de streaming</title>
+    <link rel="icon" href="../resources/img/amethyst.svg">
+    <link rel="stylesheet" href="../resources/styles/style.css">
 </head>
 <body>
     <header>
-        
+        <nav>
+            <div id="logo">
+                <a href="#">
+                    <img src="../resources/img/amethyst.svg">
+                    <h1>Amethyst</h1>
+                </a>
+            </div>
+            <ul>
+                <li class="item-nav"><a></a></li>
+                <li class="item-nav"><a></a></li>
+                <li class="item-nav"><a></a></li>
+                <li class="item-nav"><a></a></li>
+                <li class="item-nav"><a></a></li>
+            </ul>
+        </nav>
     </header>
-
+    <?php
+        include_once '../database.php';
+    ?>
     <main>
-
+        <?php
+            
+        ?>
     </main>
 </body>
 </html>
